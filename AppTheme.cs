87ec35172a1b8c4eb8d1ed240c0898224c69b23a
jkgyu7 <163077006+jkgyu7@users.noreply.github.com>
2024-04-02 11:16:09 +0800

    Add files via upload

---
+++ AppTheme.cs
@@ -0,0 +1,27 @@
+﻿using DarkMode1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThemeTutorial
+{
+    class AppTheme
+    {
+        public static void ChangeTheme(Uri themeUri)
+        {
+
+            ResourceDictionary theme = new ResourceDictionary()
+            {
+                Source = themeUri,
+            };
+
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(theme);
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThemeTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LightTheme_Click(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri("Themes/Light.xaml", UriKind.Relative));
+
+
+        }
+
+        private void DarkTheme_Click(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri("Themes/Dark.xaml", UriKind.Relative));
+        }
+    }
+}
